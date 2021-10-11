e0d19c314a79984757ebe2ea096b48f402318f08
Echo Chan <ecchochan@gmail.com>
2021-10-11 11:22:51 +0800

    update

--- setup-jupyter.sh
+++ setup-jupyter.sh
@@ -4,3 +4,5 @@ cp -f ./jupyter_setup/jupyter_notebook_config.py "$JUPYTER_PATH/jupyter_notebook
 git config --global core.attributesfile .gitattributes
 git config --global filter.dropoutput_ipynb.clean "python ./jupyter_setup/ipynb_output_filter.py"
 git config --global filter.dropoutput_ipynb.smudge cat
+
+git config --global credential.helper "cache --timeout=86400"
